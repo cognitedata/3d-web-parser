60b0a1fb68397b1699f97b05b8caf4d88dab56a6
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-16 18:37:40 +0100

    Updated sectors with more functionality

--- src/Sector.ts
+++ src/Sector.ts
@@ -1,8 +1,24 @@
+import * as THREE from 'three';
+
 export default class Sector {
+  public readonly min: THREE.Vector3;
+  public readonly max: THREE.Vector3;
+  public readonly depth: number;
+  public readonly object3d: THREE.Object3D;
   public parent: undefined | Sector;
-  constructor() {
+  public children: Array<Sector>;
+
+  constructor(min: THREE.Vector3, max: THREE.Vector3) {
+    this.min = min;
+    this.max = max;
+    this.depth = 0;
+    this.object3d = new THREE.Object3D();
+    this.object3d.frustumCulled = false;
+    this.children = [];
   }
-  addParent(parent: Sector) {
-    this.parent = parent;
+
+  addChild(child: Sector) {
+    child.parent = this;
+    this.children.push(child);
   }
 }
--- src/__tests__/Sector.test.ts
+++ src/__tests__/Sector.test.ts
@@ -1,12 +1,51 @@
+import * as THREE from 'three';
 import Sector from '../Sector';
 
 describe('Sectors', () => {
-  test('add parent', () => {
-    const parent = new Sector();
-    const child = new Sector();
-    expect(child.parent).toBe(undefined);
+  test('constructor', () => {
+    const min = new THREE.Vector3();
+    const max = new THREE.Vector3(1, 1, 1);
+    
+    const sector = new Sector(min, max);
+    expect(sector.min).toBe(min);
+    expect(sector.max).toBe(max);
+    expect(sector.parent).toBe(undefined);
+    expect(sector.children.length).toBe(0);
 
-    child.addParent(parent);
-    expect(child.parent).toBe(parent);
+    expect(sector.depth).toBe(0);
+    expect(sector.object3d.frustumCulled).toBe(false);
+  });
+
+  test('add child', () => {
+    const min = new THREE.Vector3();
+    const max = new THREE.Vector3(1, 1, 1);
+    
+    const parent = new Sector(min, max);
+    const child1 = new Sector(min, max);
+    expect(child1.parent).toBe(undefined);
+    expect(parent.children.length).toBe(0);
+
+    parent.addChild(child1);
+    expect(child1.parent).toBe(parent);
+    expect(parent.children.length).toBe(1);
+
+    const child2 = new Sector(min, max);
+    parent.addChild(child2);
+    expect(child2.parent).toBe(parent);
+    expect(parent.children.length).toBe(2);
+
+  });
+
+  test('parent bounding box', () => {
+    const parentMin = new THREE.Vector3();
+    const parentMax = new THREE.Vector3(1, 1, 1);
+    const childMin = new THREE.Vector3();
+    const childMax = new THREE.Vector3(0.5, 0.5, 0.5);
+    const parent = new Sector(parentMin, parentMax);
+    const child = new Sector(childMin, childMax);
+    
+    parent.addChild(child);
+    expect(parent.max.clone().sub(parent.min).length())
+      .toBeGreaterThanOrEqual(child.max.clone().sub(child.min).length());
   });
 });
