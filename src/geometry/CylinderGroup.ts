7c1238ca1e7bf8f2124b4751f8671ce27ea18720
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-17 10:57:44 +0100

    Added CylinderGroup with tests

--- src/geometry/CylinderGroup.ts
+++ src/geometry/CylinderGroup.ts
@@ -1,28 +1,49 @@
 import * as THREE from 'three';
-import PrimitiveGroup from './PrimitiveGroup';
+import BaseCylinderGroup from './BaseCylinderGroup';
 
-export default abstract class CylinderGroup extends PrimitiveGroup {
-  centerA: Float32Array;
-  centerB: Float32Array;
+export default class CylinderGroup extends BaseCylinderGroup {
+    static type = 'Cylinder';
+    radius: Float32Array;
+    isClosed: Uint8Array;
   constructor(capacity: number) {
     super(capacity);
-    this.centerA = new Float32Array(3 * capacity);
-    this.centerB = new Float32Array(3 * capacity);
+    this.radius = new Float32Array(capacity);
+    this.isClosed = new Uint8Array(capacity);
   }
 
-  setCenterA(value: THREE.Vector3, index: number) {
-    this.setVector(value, this.centerA, index);
+  setRadius(value: number, index: number) {
+    this.radius[index] = value;
   }
 
-  getCenterA(target: THREE.Vector3, index: number): THREE.Vector3 {
-    return this.getVector(this.centerA, target, index);
+  getRadius(index: number): number {
+    return this.radius[index];
   }
 
-  setCenterB(value: THREE.Vector3, index: number) {
-    this.setVector(value, this.centerB, index);
+  setIsClosed(value: boolean, index: number) {
+    this.isClosed[index] = value ? 1 : 0;
   }
 
-  getCenterB(target: THREE.Vector3, index: number): THREE.Vector3 {
-    return this.getVector(this.centerB, target, index);
+  getIsClosed(index: number): boolean {
+    return this.isClosed[index] === 1 ? true : false;
+  }
+
+  add(
+    nodeId: number,
+    treeIndex: number,
+    color: THREE.Color,
+    centerA: THREE.Vector3,
+    centerB: THREE.Vector3,
+    radius: number,
+    isClosed: boolean,
+  ) {
+    this.setNodeId(nodeId, this.count);
+    this.setTreeIndex(treeIndex, this.count);
+    this.setColor(color, this.count);
+    this.setVector(centerA, this.centerA, this.count);
+    this.setVector(centerB, this.centerB, this.count);
+    this.setRadius(radius, this.count);
+    this.setIsClosed(isClosed, this.count);
+
+    this.count += 1;
   }
 }
