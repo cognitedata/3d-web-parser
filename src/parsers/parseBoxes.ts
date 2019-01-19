52de6da0d34f8634bb1cec5e90cb0565e2486aae
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-19 11:37:21 +0100

    Added parsers (copied from circle)

---
+++ src/parsers/parseBoxes.ts
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+import BoxGroup from '../geometry/BoxGroup';
+import { parsePrimitiveColor, parsePrimitiveInfo, parsePrimitiveNodeId, parsePrimitiveTreeIndex } from './parseUtils';
+
+const color = new THREE.Color();
+const vector1 = new THREE.Vector3();
+const vector2 = new THREE.Vector3();
+const vector3 = new THREE.Vector3();
+
+function count(geometries: any[]): number {
+  return geometries.reduce( (total, geometry) => { return geometry.type === 'box' ? total + 1 : total; }, 0);
+}
+
+export default function parseBoxes(geometries: any[]): BoxGroup|null {
+  const group = new BoxGroup(count(geometries));
+  if (group.capacity === 0) {
+    return null;
+  }
+
+  const boxes = geometries.filter(object => object.type === 'box');
+  boxes.forEach(geometry => {
+    const primitiveInfo = parsePrimitiveInfo(geometry.primitiveInfo);
+
+    const nodeId = parsePrimitiveNodeId(geometry);
+    const treeIndex = parsePrimitiveTreeIndex(geometry);
+    const center = geometry.primitiveInfo.box.center;
+    const normal = geometry.primitiveInfo.box.normal;
+    const delta = geometry.primitiveInfo.box.delta;
+    const angle = geometry.primitiveInfo.box.angle;
+
+    vector1.set(center.x, center.y, center.z);
+    vector2.set(normal.x, normal.y, normal.z);
+    vector3.set(delta.x, delta.y, delta.z);
+    color.setHex(parsePrimitiveColor(geometry));
+
+    group.add(nodeId, treeIndex, color, vector1, vector2, angle, vector3);
+  });
+  return group;
+}
--- src/parsers/parseTrapeziums.ts
+++ src/parsers/parseTrapeziums.ts
@@ -7,7 +7,7 @@ const vector1 = new THREE.Vector3();
 const vector2 = new THREE.Vector3();
 const vector3 = new THREE.Vector3();
 
-function count(geometries: any[]): number {
+function countCircles(geometries: any[]): number {
   const numCircles = geometries.reduce(
     (total, geometry) => { return geometry.type === 'circle' ? total + 1 : total; }, 0);
 
@@ -25,25 +25,28 @@ function count(geometries: any[]): number {
 }
 
 export default function parseCircles(geometries: any[]): CircleGroup|null {
-  const group = new CircleGroup(count(geometries));
-  if (group.capacity === 0) {
+  const numCircles = countCircles(geometries);
+  if (numCircles === 0) {
     return null;
   }
   const circles = geometries.filter(object => object.type === 'circle');
-  // TODO: add handling for cylinders etc
+  // const circles = geometries.filter(object => object.type === 'circle');
 
-  circles.forEach(geometry => {
-    const primitiveInfo = parsePrimitiveInfo(geometry.primitiveInfo);
+  const count = circles.length;
+  const group = new CircleGroup(count);
 
-    const nodeId = parsePrimitiveNodeId(geometry);
-    const treeIndex = parsePrimitiveTreeIndex(geometry);
-    const center = geometry.primitiveInfo.circle.center;
-    const normal = geometry.primitiveInfo.circle.normal;
-    const radius = geometry.primitiveInfo.circle.radius;
+  circles.forEach(circle => {
+    const primitiveInfo = parsePrimitiveInfo(circle.primitiveInfo);
+
+    const nodeId = parsePrimitiveNodeId(circle);
+    const treeIndex = parsePrimitiveTreeIndex(circle);
+    const center = circle.primitiveInfo.circle.center;
+    const normal = circle.primitiveInfo.circle.normal;
+    const radius = circle.primitiveInfo.circle.radius;
     vector1.set(center.x, center.y, center.z);
     vector2.set(normal.x, normal.y, normal.z);
-    color.setHex(parsePrimitiveColor(geometry));
-
+    console.log('Color thing: ', parsePrimitiveColor(circle));
+    color.setHex(parsePrimitiveColor(circle));
     group.add(nodeId, treeIndex, color, vector1, vector2, radius);
   });
   return group;
