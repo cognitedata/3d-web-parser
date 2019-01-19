e601e2d1f785129be8c2e5f11026dd1debf362a0
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-19 15:48:42 +0100

    Added test for parseGeneralRing

--- src/__tests__/parseGeneralRings.test.ts
+++ src/__tests__/parseGeneralRings.test.ts
@@ -1,48 +1,61 @@
 // Copyright 2019 Cognite AS
 import * as THREE from 'three';
-import parseBoxes from '../parsers/parseBoxes';
+import parseGeneralRings from '../parsers/parseGeneralRings';
 import { expectVector3Equal, expectColorEqual } from '../TestUtils';
-import BoxGroup from '../geometry/BoxGroup';
+import GeneralRingGroup from '../geometry/GeneralRingGroup';
 
-describe('parseBoxes', () => {
-  test('parseBoxes', () => {
+describe('parseGeneralRings', () => {
+  test('parseGeneralRings', () => {
     const nodeId = 1234;
     const treeIndex = 123;
     const color = new THREE.Color(1.0, 0.3, 0.4);
     const center = new THREE.Vector3(1.0, 2.0, 0.0);
     const normal = new THREE.Vector3(5.0, 10.0, 3.0);
-    const delta = new THREE.Vector3(12, 15.1, 10.32);
+    const xRadius = 1.0;
+    const yRadius = 1.0;
+    const localXAxis = new THREE.Vector3(6.0, 11.0, 4.0);
+    const thickness = 1.0;
     const angle = 1.0;
+    const arcAngle = 1.0;
 
     const geometries = [
       {
-        type: 'box',
+        type: 'generalRing',
         nodes: [{ properties: [{
           nodeId: nodeId,
           treeIndex: treeIndex,
           color: { rgb: color.getHex() },
         }] }],
         primitiveInfo: {
-          box: {
+          generalRing: {
             center: { x: center.x, y: center.y, z: center.z },
             normal: { x: normal.x, y: normal.y, z: normal.z },
-            delta: { x: delta.x, y: delta.y, z: delta.z },
+            xRadius: xRadius,
+            yRadius: yRadius,
+            localXAxis: localXAxis,
+            thickness: thickness,
             angle: angle,
+            arcAngle: arcAngle,
           },
         },
       },
     ];
 
-    let group: BoxGroup;
+    let group: GeneralRingGroup;
     // @ts-ignore
-    group = parseBoxes(geometries);
+    group = parseGeneralRings(geometries);
     expect(group.capacity).toBe(1);
-    expectColorEqual(group.getColor(new THREE.Color(), 0), color);
-    expectVector3Equal(group.getCenter(new THREE.Vector3(), 0), center);
-    expectVector3Equal(group.getNormal(new THREE.Vector3(), 0), normal);
-    expectVector3Equal(group.getDelta(new THREE.Vector3(), 0), delta);
-    expect(group.getAngle(0)).toBeCloseTo(angle);
     expect(group.getNodeId(0)).toBe(nodeId);
     expect(group.getTreeIndex(0)).toBe(treeIndex);
+    expectColorEqual(group.getColor(new THREE.Color(), 0), color);
+
+    expectVector3Equal(group.getCenter(new THREE.Vector3(), 0), center);
+    expectVector3Equal(group.getNormal(new THREE.Vector3(), 0), normal);
+    expect(group.getXRadius(0)).toBe(xRadius);
+    expect(group.getYRadius(0)).toBe(yRadius);
+    expectVector3Equal(group.getLocalXAxis(new THREE.Vector3(), 0), localXAxis);
+    expect(group.getThickness(0)).toBe(thickness);
+    expect(group.getAngle(0)).toBe(angle);
+    expect(group.getArcAngle(0)).toBe(arcAngle);
   });
 });
