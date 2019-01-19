c9670dfc59b1270c500b6749e12970ef0351a1e9
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-19 15:45:58 +0100

    Added test for parseGeneralCylinder

--- src/__tests__/parseGeneralCylinders.test.ts
+++ src/__tests__/parseGeneralCylinders.test.ts
@@ -1,48 +1,70 @@
 // Copyright 2019 Cognite AS
 import * as THREE from 'three';
-import parseBoxes from '../parsers/parseBoxes';
+import parseGeneralCylinders from '../parsers/parseGeneralCylinders';
 import { expectVector3Equal, expectColorEqual } from '../TestUtils';
-import BoxGroup from '../geometry/BoxGroup';
+import GeneralCylinderGroup from '../geometry/GeneralCylinderGroup';
 
-describe('parseBoxes', () => {
-  test('parseBoxes', () => {
+describe('parseGeneralCylinders', () => {
+  test('parseGeneralCylinders', () => {
     const nodeId = 1234;
     const treeIndex = 123;
     const color = new THREE.Color(1.0, 0.3, 0.4);
-    const center = new THREE.Vector3(1.0, 2.0, 0.0);
-    const normal = new THREE.Vector3(5.0, 10.0, 3.0);
-    const delta = new THREE.Vector3(12, 15.1, 10.32);
-    const angle = 1.0;
+    const centerA = new THREE.Vector3(1.0, 2.0, 0.0);
+    const centerB = new THREE.Vector3(1.0, 2.0, 0.0);
+    const radius = 2.0;
+    const heightA = 3.0;
+    const heightB = 4.0;
+    const slopeA = 5.0;
+    const slopeB = 6.0;
+    const zAngleA = 7.0;
+    const zAngleB = 8.0;
+    const angle = 1.5;
+    const arcAngle = 1.4;
 
     const geometries = [
       {
-        type: 'box',
+        type: 'generalCylinder',
         nodes: [{ properties: [{
           nodeId: nodeId,
           treeIndex: treeIndex,
           color: { rgb: color.getHex() },
         }] }],
         primitiveInfo: {
-          box: {
-            center: { x: center.x, y: center.y, z: center.z },
-            normal: { x: normal.x, y: normal.y, z: normal.z },
-            delta: { x: delta.x, y: delta.y, z: delta.z },
+          generalCylinder: {
+            centerA: { x: centerA.x, y: centerA.y, z: centerA.z },
+            centerB: { x: centerB.x, y: centerB.y, z: centerB.z },
+            radius: radius,
+            heightA: heightA,
+            heightB: heightB,
+            slopeA: slopeA,
+            slopeB: slopeB,
+            zAngleA: zAngleA,
+            zAngleB: zAngleB,
             angle: angle,
+            arcAngle: arcAngle,
           },
         },
       },
     ];
 
-    let group: BoxGroup;
+    let group: GeneralCylinderGroup;
     // @ts-ignore
-    group = parseBoxes(geometries);
+    group = parseGeneralCylinders(geometries);
     expect(group.capacity).toBe(1);
-    expectColorEqual(group.getColor(new THREE.Color(), 0), color);
-    expectVector3Equal(group.getCenter(new THREE.Vector3(), 0), center);
-    expectVector3Equal(group.getNormal(new THREE.Vector3(), 0), normal);
-    expectVector3Equal(group.getDelta(new THREE.Vector3(), 0), delta);
-    expect(group.getAngle(0)).toBeCloseTo(angle);
     expect(group.getNodeId(0)).toBe(nodeId);
     expect(group.getTreeIndex(0)).toBe(treeIndex);
+    expectColorEqual(group.getColor(new THREE.Color(), 0), color);
+
+    expectVector3Equal(group.getCenterA(new THREE.Vector3(), 0), centerA);
+    expectVector3Equal(group.getCenterB(new THREE.Vector3(), 0), centerB);
+    expect(group.getRadius(0)).toBeCloseTo(radius);
+    expect(group.getHeightA(0)).toBeCloseTo(heightA);
+    expect(group.getHeightB(0)).toBeCloseTo(heightB);
+    expect(group.getSlopeA(0)).toBeCloseTo(slopeA);
+    expect(group.getSlopeB(0)).toBeCloseTo(slopeB);
+    expect(group.getZAngleA(0)).toBeCloseTo(zAngleA);
+    expect(group.getZAngleB(0)).toBeCloseTo(zAngleB);
+    expect(group.getAngle(0)).toBeCloseTo(angle);
+    expect(group.getArcAngle(0)).toBeCloseTo(arcAngle);
   });
 });
