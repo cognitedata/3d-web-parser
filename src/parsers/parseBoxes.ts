86935da93e6f02db43129c67d447d46975e0b0bd
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-20 19:05:46 +0100

    More parsers

--- src/parsers/parseBoxes.ts
+++ src/parsers/parseBoxes.ts
@@ -1,14 +1,14 @@
 import * as THREE from 'three';
 import BoxGroup from '../geometry/BoxGroup';
 import { parsePrimitiveColor, parsePrimitiveInfo, parsePrimitiveNodeId, parsePrimitiveTreeIndex } from './parseUtils';
-
+const type = 'box';
 const color = new THREE.Color();
 const vector1 = new THREE.Vector3();
 const vector2 = new THREE.Vector3();
 const vector3 = new THREE.Vector3();
 
 function count(geometries: any[]): number {
-  return geometries.reduce( (total, geometry) => { return geometry.type === 'box' ? total + 1 : total; }, 0);
+  return geometries.reduce( (total, geometry) => { return geometry.type === type ? total + 1 : total; }, 0);
 }
 
 export default function parseBoxes(geometries: any[]): BoxGroup|null {
@@ -17,8 +17,8 @@ export default function parseBoxes(geometries: any[]): BoxGroup|null {
     return null;
   }
 
-  const boxes = geometries.filter(object => object.type === 'box');
-  boxes.forEach(geometry => {
+  const objects = geometries.filter(geometry => geometry.type === type);
+  objects.forEach(geometry => {
     const primitiveInfo = parsePrimitiveInfo(geometry.primitiveInfo);
 
     const nodeId = parsePrimitiveNodeId(geometry);
