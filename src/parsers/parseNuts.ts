b37deae66fa3faf3fcd10ce525c2f18409e93143
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-20 12:43:53 +0100

    Parsing nuts

--- src/parsers/parseNuts.ts
+++ src/parsers/parseNuts.ts
@@ -1,53 +1,40 @@
 import * as THREE from 'three';
-import CircleGroup from '../geometry/CircleGroup';
+import NutGroup from '../geometry/NutGroup';
 import { parsePrimitiveColor, parsePrimitiveInfo, parsePrimitiveNodeId, parsePrimitiveTreeIndex } from './parseUtils';
+const type = 'nut';
 
 const color = new THREE.Color();
 const vector1 = new THREE.Vector3();
 const vector2 = new THREE.Vector3();
 const vector3 = new THREE.Vector3();
 
-function countCircles(geometries: any[]): number {
-  const numCircles = geometries.reduce(
-    (total, geometry) => { return geometry.type === 'circle' ? total + 1 : total; }, 0);
-
-  const numExtraCircles = geometries.reduce((total, geometry) => {
-    if (['cylinder', 'cone', 'eccentricCone'].indexOf(geometry.type) > 0) {
-      // Found one of them
-      const type = Object.keys(geometry.primitiveInfo)[0];
-      const isClosed = geometry.primitiveInfo[type].isClosed;
-      return total + 2;
-    }
-    return total;
-  }, 0);
-
-  return numCircles + numExtraCircles;
+function count(geometries: any[]): number {
+  return geometries.reduce(
+    (total, geometry) => { return geometry.type === type ? total + 1 : total; }, 0);
 }
 
-export default function parseCircles(geometries: any[]): CircleGroup|null {
-  const numCircles = countCircles(geometries);
-  if (numCircles === 0) {
+export default function parse(geometries: any[]): NutGroup|null {
+  const group = new NutGroup(count(geometries));
+  if (group.capacity === 0) {
     return null;
   }
-  const circles = geometries.filter(object => object.type === 'circle');
-  // const circles = geometries.filter(object => object.type === 'circle');
+  const objects = geometries.filter(geometry => geometry.type === type);
 
-  const count = circles.length;
-  const group = new CircleGroup(count);
+  objects.forEach(geometry => {
+    const primitiveInfo = parsePrimitiveInfo(geometry.primitiveInfo);
 
-  circles.forEach(circle => {
-    const primitiveInfo = parsePrimitiveInfo(circle.primitiveInfo);
+    const nodeId = parsePrimitiveNodeId(geometry);
+    const treeIndex = parsePrimitiveTreeIndex(geometry);
+    color.setHex(parsePrimitiveColor(geometry));
 
-    const nodeId = parsePrimitiveNodeId(circle);
-    const treeIndex = parsePrimitiveTreeIndex(circle);
-    const center = circle.primitiveInfo.circle.center;
-    const normal = circle.primitiveInfo.circle.normal;
-    const radius = circle.primitiveInfo.circle.radius;
-    vector1.set(center.x, center.y, center.z);
-    vector2.set(normal.x, normal.y, normal.z);
-    console.log('Color thing: ', parsePrimitiveColor(circle));
-    color.setHex(parsePrimitiveColor(circle));
-    group.add(nodeId, treeIndex, color, vector1, vector2, radius);
+    const centerA = geometry.primitiveInfo[geometry.type].centerA;
+    const centerB = geometry.primitiveInfo[geometry.type].centerB;
+    const radius = geometry.primitiveInfo[geometry.type].radius;
+    const rotationAngle = geometry.primitiveInfo[geometry.type].rotationAngle;
+
+    vector1.set(centerA.x, centerA.y, centerA.z);
+    vector2.set(centerB.x, centerB.y, centerB.z);
+    group.add(nodeId, treeIndex, color, centerA, centerB, radius, rotationAngle);
   });
   return group;
 }
