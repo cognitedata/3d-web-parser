2f580c229766ac31e86427ed398e41e4c7a9f519
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-22 13:00:26 +0100

    Added parseSphericalSegments

---
+++ src/parsers/parseSphericalSegments.ts
@@ -0,0 +1,50 @@
+import * as THREE from 'three';
+import SphericalSegmentGroup from '../geometry/SphericalSegmentGroup';
+import { MatchingGeometries, parsePrimitiveColor, parsePrimitiveNodeId, parsePrimitiveTreeIndex } from './parseUtils';
+
+const color = new THREE.Color();
+const center = new THREE.Vector3();
+const normal = new THREE.Vector3();
+
+function findMatchingGeometries(geometries: any[]): MatchingGeometries {
+  const matchingGeometries: MatchingGeometries = {
+    count: 0,
+    geometries: [],
+  };
+
+  geometries.forEach(geometry => {
+    if (geometry.type === 'sphere' || geometry.type === 'sphericalSegment') {
+      matchingGeometries.geometries.push(geometry);
+      matchingGeometries.count += 1;
+    }
+  });
+
+  return matchingGeometries;
+}
+
+export default function parse(geometries: any[]): SphericalSegmentGroup|null {
+  const matchingGeometries = findMatchingGeometries(geometries);
+  const group = new SphericalSegmentGroup(matchingGeometries.count);
+  if (group.capacity === 0) {
+    return null;
+  }
+
+  matchingGeometries.geometries.forEach(geometry => {
+    const primitiveInfo = geometry.primitiveInfo[geometry.type];
+    const nodeId = parsePrimitiveNodeId(geometry);
+    const treeIndex = parsePrimitiveTreeIndex(geometry);
+    color.setHex(parsePrimitiveColor(geometry));
+
+    center.set(primitiveInfo.center.x, primitiveInfo.center.y, primitiveInfo.center.z);
+    normal.set(primitiveInfo.normal.x, primitiveInfo.normal.y, primitiveInfo.normal.z);
+    const radius = primitiveInfo.radius;
+    let height = 2 * radius; // Default value for sphere
+
+    if (geometry.type === 'sphericalSegment') {
+      height = primitiveInfo.height;
+    }
+
+    group.add(nodeId, treeIndex, color, center, normal, radius, height);
+  });
+  return group;
+}
