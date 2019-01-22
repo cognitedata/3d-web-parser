9cab39208531c94879fbd8d581ba1591b8bcd896
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-22 13:35:18 +0100

    Implemented parseTrapezium

--- src/parsers/parseTrapeziums.ts
+++ src/parsers/parseTrapeziums.ts
@@ -1,53 +1,110 @@
 import * as THREE from 'three';
-import CircleGroup from '../geometry/CircleGroup';
-import { parsePrimitiveColor, parsePrimitiveInfo, parsePrimitiveNodeId, parsePrimitiveTreeIndex } from './parseUtils';
+import TrapeziumGroup from '../geometry/TrapeziumGroup';
+import { MatchingGeometries, parsePrimitiveColor, parsePrimitiveNodeId, parsePrimitiveTreeIndex } from './parseUtils';
+import { xAxis, zAxis } from '../constants';
 
-const color = new THREE.Color();
-const vector1 = new THREE.Vector3();
-const vector2 = new THREE.Vector3();
-const vector3 = new THREE.Vector3();
+const THREEColor = new THREE.Color();
+const centerA = new THREE.Vector3();
+const centerB = new THREE.Vector3();
+const capZAxis = new THREE.Vector3();
+const capXAxis = new THREE.Vector3();
+const vertex = new THREE.Vector3();
+const normal = new THREE.Vector3();
+const localXAxis = new THREE.Vector3();
+const rotation = new THREE.Quaternion();
 
-function countCircles(geometries: any[]): number {
-  const numCircles = geometries.reduce(
-    (total, geometry) => { return geometry.type === 'circle' ? total + 1 : total; }, 0);
+function findMatchingGeometries(geometries: any[]): MatchingGeometries {
+  const matchingGeometries: MatchingGeometries = {
+    count: 0,
+    geometries: [],
+  };
 
-  const numExtraCircles = geometries.reduce((total, geometry) => {
-    if (['cylinder', 'cone', 'eccentricCone'].indexOf(geometry.type) > 0) {
-      // Found one of them
-      const type = Object.keys(geometry.primitiveInfo)[0];
-      const isClosed = geometry.primitiveInfo[type].isClosed;
-      return total + 2;
+  geometries.forEach(geometry => {
+    const { thickness, arcAngle, isClosed } = geometry.primitiveInfo[geometry.type];
+
+    if ( (geometry.type === 'cone' || geometry.type === 'generalCylinder')
+        && thickness > 0
+        && arcAngle < 2 * Math.PI
+        && isClosed) {
+      matchingGeometries.geometries.push(geometry);
+      matchingGeometries.count += 2;
     }
-    return total;
-  }, 0);
+  });
 
-  return numCircles + numExtraCircles;
+  return matchingGeometries;
 }
 
-export default function parseCircles(geometries: any[]): CircleGroup|null {
-  const numCircles = countCircles(geometries);
-  if (numCircles === 0) {
+function parseCone(primitiveInfo: any,
+                   nodeId: number,
+                   treeIndex: number,
+                   color: THREE.Color,
+                   group: TrapeziumGroup) {
+  centerA.set(primitiveInfo.centerA.x, primitiveInfo.centerA.y, primitiveInfo.centerA.z);
+  centerB.set(primitiveInfo.centerB.x, primitiveInfo.centerB.y, primitiveInfo.centerB.z);
+
+  capZAxis.copy(centerA).sub(centerB);
+  rotation.setFromUnitVectors(zAxis, capZAxis.normalize());
+  capXAxis.copy(xAxis).applyQuaternion(rotation);
+  const {
+    angle = 0,
+    arcAngle,
+    isClosed,
+    radiusA,
+    radiusB,
+    thickness = 0,
+  } = primitiveInfo;
+
+  [false, true].forEach(isSecondQuad => {
+    const finalAngle = angle + Number(isSecondQuad) * arcAngle;
+    vertex
+      .set(Math.cos(finalAngle), Math.sin(finalAngle), 0)
+      .applyQuaternion(rotation)
+      .normalize();
+    const vertices: THREE.Vector3[] = [];
+    const offsets = [0, -thickness];
+    [true, false].forEach(isA => {
+      if (isSecondQuad) { isA = !isA; }
+      const radius = isA ? radiusA : radiusB;
+      const center = isA ? centerA : centerB;
+      offsets.forEach(offset => {
+        vertices.push(
+          vertex
+            .clone()
+            .multiplyScalar(radius + offset)
+            .add(center),
+        );
+      });
+    });
+    group.add(nodeId, treeIndex, color, vertices[0], vertices[1], vertices[2], vertices[3]);
+  });
+}
+
+function parseGeneralCylinder(primitiveInfo: any,
+                              nodeId: number,
+                              treeIndex: number,
+                              color: THREE.Color,
+                              group: TrapeziumGroup) {
+  console.log('Trapezium parsing from generalCylinder parsing isn\'t implemented');
+}
+
+export default function parse(geometries: any[]): TrapeziumGroup|null {
+  const matchingGeometries = findMatchingGeometries(geometries);
+  const group = new TrapeziumGroup(matchingGeometries.count);
+  if (group.capacity === 0) {
     return null;
   }
-  const circles = geometries.filter(object => object.type === 'circle');
-  // const circles = geometries.filter(object => object.type === 'circle');
 
-  const count = circles.length;
-  const group = new CircleGroup(count);
+  matchingGeometries.geometries.forEach(geometry => {
+    const primitiveInfo = geometry.primitiveInfo[geometry.type];
+    const nodeId = parsePrimitiveNodeId(geometry);
+    const treeIndex = parsePrimitiveTreeIndex(geometry);
+    THREEColor.setHex(parsePrimitiveColor(geometry));
 
-  circles.forEach(circle => {
-    const primitiveInfo = parsePrimitiveInfo(circle.primitiveInfo);
-
-    const nodeId = parsePrimitiveNodeId(circle);
-    const treeIndex = parsePrimitiveTreeIndex(circle);
-    const center = circle.primitiveInfo.circle.center;
-    const normal = circle.primitiveInfo.circle.normal;
-    const radius = circle.primitiveInfo.circle.radius;
-    vector1.set(center.x, center.y, center.z);
-    vector2.set(normal.x, normal.y, normal.z);
-    console.log('Color thing: ', parsePrimitiveColor(circle));
-    color.setHex(parsePrimitiveColor(circle));
-    group.add(nodeId, treeIndex, color, vector1, vector2, radius);
+    if (geometry.type === 'cone') {
+      parseCone(primitiveInfo, nodeId, treeIndex, THREEColor, group);
+    } else if (geometry.type === 'generalCylinder') {
+      parseGeneralCylinder(primitiveInfo, nodeId, treeIndex, THREEColor, group);
+    }
   });
   return group;
 }
