684ac75f8be373e48ad7510ce775e7f44ed3b754
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-22 12:22:13 +0100

    Updated parseBox

--- src/parsers/parseBoxes.ts
+++ src/parsers/parseBoxes.ts
@@ -1,16 +1,11 @@
 import * as THREE from 'three';
 import BoxGroup from '../geometry/BoxGroup';
-import { MatchingGeometries,
-  parsePrimitiveColor,
-  parsePrimitiveInfo,
-  parsePrimitiveNodeId,
-  parsePrimitiveTreeIndex,
-} from './parseUtils';
+import { MatchingGeometries, parsePrimitiveColor, parsePrimitiveNodeId, parsePrimitiveTreeIndex } from './parseUtils';
 
 const color = new THREE.Color();
-const vector1 = new THREE.Vector3();
-const vector2 = new THREE.Vector3();
-const vector3 = new THREE.Vector3();
+const center = new THREE.Vector3();
+const normal = new THREE.Vector3();
+const delta = new THREE.Vector3();
 
 function findMatchingGeometries(geometries: any[]): MatchingGeometries {
   const matchingGeometries: MatchingGeometries = {
@@ -36,22 +31,18 @@ export default function parseBoxes(geometries: any[]): BoxGroup|null {
   }
 
   matchingGeometries.geometries.forEach(geometry => {
-    const primitiveInfo = parsePrimitiveInfo(geometry.primitiveInfo);
+    const primitiveInfo = geometry.primitiveInfo[geometry.type];
 
     const nodeId = parsePrimitiveNodeId(geometry);
     const treeIndex = parsePrimitiveTreeIndex(geometry);
     color.setHex(parsePrimitiveColor(geometry));
 
-    const center = geometry.primitiveInfo.box.center;
-    const normal = geometry.primitiveInfo.box.normal;
-    const delta = geometry.primitiveInfo.box.delta;
+    center.set(primitiveInfo.center.x, primitiveInfo.center.y, primitiveInfo.center.z);
+    normal.set(primitiveInfo.normal.x, primitiveInfo.normal.y, primitiveInfo.normal.z);
+    delta.set(primitiveInfo.delta.x, primitiveInfo.delta.y, primitiveInfo.delta.z);
     const angle = geometry.primitiveInfo.box.angle;
 
-    vector1.set(center.x, center.y, center.z);
-    vector2.set(normal.x, normal.y, normal.z);
-    vector3.set(delta.x, delta.y, delta.z);
-
-    group.add(nodeId, treeIndex, color, vector1, vector2, angle, vector3);
+    group.add(nodeId, treeIndex, color, center, normal, angle, delta);
   });
   return group;
 }
