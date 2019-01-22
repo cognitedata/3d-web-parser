8a1fd4471fa484cf8b62832afc031f7801257333
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-22 10:59:00 +0100

    Implemented parseEllipsoidSegment

--- src/geometry/EllipsoidSegmentGroup.ts
+++ src/geometry/EllipsoidSegmentGroup.ts
@@ -1,16 +1,18 @@
 // Copyright 2019 Cognite AS
 
 import * as THREE from 'three';
-import EllipsoidGroup from './EllipsoidGroup';
+import PlaneGroup from './PlaneGroup';
 
-export default class EllipsoidSegmentGroup extends EllipsoidGroup {
+export default class EllipsoidSegmentGroup extends PlaneGroup {
   static type = 'EllipsoidSegment';
+  public horizontalRadius: Float32Array;
+  public verticalRadius: Float32Array;
   public height: Float32Array;
-  public isClosed: Uint8Array;
   constructor(capacity: number) {
     super(capacity);
     this.height = new Float32Array(capacity);
-    this.isClosed = new Uint8Array(capacity);
+    this.horizontalRadius = new Float32Array(capacity);
+    this.verticalRadius = new Float32Array(capacity);
   }
 
   setHeight(value: number, index: number) {
@@ -21,12 +23,20 @@ export default class EllipsoidSegmentGroup extends EllipsoidGroup {
     return this.height[index];
   }
 
-  setIsClosed(value: boolean, index: number) {
-    this.isClosed[index] = value ? 1 : 0;
+  setHorizontalRadius(value: number, index: number) {
+    this.horizontalRadius[index] = value;
   }
 
-  getIsClosed(index: number): boolean {
-    return this.isClosed[index] === 1 ? true : false;
+  getHorizontalRadius(index: number): number {
+    return this.horizontalRadius[index];
+  }
+
+  setVerticalRadius(value: number, index: number) {
+    this.verticalRadius[index] = value;
+  }
+
+  getVerticalRadius(index: number): number {
+    return this.verticalRadius[index];
   }
 
   // @ts-ignore
@@ -36,20 +46,18 @@ export default class EllipsoidSegmentGroup extends EllipsoidGroup {
     color: THREE.Color,
     center: THREE.Vector3,
     normal: THREE.Vector3,
-    hRadius: number,
-    vRadius: number,
+    horizontalRadius: number,
+    verticalRadius: number,
     height: number,
-    isClosed: boolean,
   ) {
     this.setNodeId(nodeId, this.count);
     this.setTreeIndex(treeIndex, this.count);
     this.setColor(color, this.count);
     this.setCenter(center, this.count);
     this.setNormal(normal, this.count);
-    this.setHRadius(hRadius, this.count);
-    this.setVRadius(vRadius, this.count);
+    this.setHorizontalRadius(horizontalRadius, this.count);
+    this.setVerticalRadius(verticalRadius, this.count);
     this.setHeight(height, this.count);
-    this.setIsClosed(isClosed, this.count);
     this.count += 1;
   }
 
--- src/parsers/parseEllipsoidSegments.ts
+++ src/parsers/parseEllipsoidSegments.ts
@@ -1,46 +1,59 @@
 import * as THREE from 'three';
 import EllipsoidSegmentGroup from '../geometry/EllipsoidSegmentGroup';
 import { parsePrimitiveColor, parsePrimitiveInfo, parsePrimitiveNodeId, parsePrimitiveTreeIndex } from './parseUtils';
-const type = 'ellipsoidSegments';
-const color = new THREE.Color();
-const vector1 = new THREE.Vector3();
-const vector2 = new THREE.Vector3();
-const vector3 = new THREE.Vector3();
 
-function count(geometries: any[]): number {
-  return geometries.reduce(
-    (total, geometry) => { return [type, 'ellipsoid'].indexOf(geometry.type) > -1 ? total + 1 : total; }, 0);
+const color = new THREE.Color();
+const center = new THREE.Vector3();
+const normal = new THREE.Vector3();
+
+interface MatchingGeometries {
+  count: number;
+  geometries: any[];
+}
+
+function findMatchingGeometries(geometries: any[]): MatchingGeometries {
+  const matchingGeometries: MatchingGeometries = {
+    count: 0,
+    geometries: [],
+  };
+
+  geometries.forEach(geometry => {
+    const isClosed = geometry.primitiveInfo[geometry.type].isClosed;
+    const thickness = geometry.primitiveInfo[geometry.type].thickness;
+
+    if (geometry.type === 'ellipsoid' || geometry.type === 'ellipsoidSegment') {
+      matchingGeometries.geometries.push(geometry);
+      matchingGeometries.count += 1;
+    }
+  });
+
+  return matchingGeometries;
 }
 
 export default function parse(geometries: any[]): EllipsoidSegmentGroup|null {
-  const group = new EllipsoidSegmentGroup(count(geometries));
+  const matchingGeometries = findMatchingGeometries(geometries);
+  const group = new EllipsoidSegmentGroup(matchingGeometries.count);
   if (group.capacity === 0) {
     return null;
   }
-  const objects = geometries.filter(geometry => [type, 'ellipsoid'].indexOf(geometry.type) > -1);
 
-  objects.forEach(geometry => {
-    const primitiveInfo = parsePrimitiveInfo(geometry.primitiveInfo);
-
+  matchingGeometries.geometries.forEach(geometry => {
+    const primitiveInfo = geometry.primitiveInfo[geometry.type];
     const nodeId = parsePrimitiveNodeId(geometry);
     const treeIndex = parsePrimitiveTreeIndex(geometry);
-    const center = geometry.primitiveInfo[geometry.type].center;
-    const normal = geometry.primitiveInfo[geometry.type].normal;
-    const hRadius = geometry.primitiveInfo[geometry.type].horizontalRadius;
-    const vRadius = geometry.primitiveInfo[geometry.type].verticalRadius;
-    let height = 2 * vRadius;
-    let isClosed = false;
+    color.setHex(parsePrimitiveColor(geometry));
+
+    center.set(primitiveInfo.center.x, primitiveInfo.center.y, primitiveInfo.center.z);
+    normal.set(primitiveInfo.normal.x, primitiveInfo.normal.y, primitiveInfo.normal.z);
+    const horizontalRadius = primitiveInfo.horizontalRadius;
+    const verticalRadius = primitiveInfo.verticalRadius;
+    let height = 2 * verticalRadius; // Default value for ellipsoids
 
     if (geometry.type === 'ellipsoidSegment') {
-      // ellipsoidSegment has two more properties
-      height = geometry.primitiveInfo[geometry.type].height;
-      isClosed = geometry.primitiveInfo[geometry.type].isClosed;
+      height = primitiveInfo.height;
     }
 
-    vector1.set(center.x, center.y, center.z);
-    vector2.set(normal.x, normal.y, normal.z);
-    color.setHex(parsePrimitiveColor(geometry));
-    group.add(nodeId, treeIndex, color, vector1, vector2, hRadius, vRadius, height, isClosed);
+    group.add(nodeId, treeIndex, color, center, normal, horizontalRadius, verticalRadius, height);
   });
   return group;
 }
