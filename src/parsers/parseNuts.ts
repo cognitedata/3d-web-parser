0277e3fd9e9c31985979b677f5321d61ac0f3179
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-22 12:22:20 +0100

    Updated parseNuts

--- src/parsers/parseNuts.ts
+++ src/parsers/parseNuts.ts
@@ -1,40 +1,47 @@
 import * as THREE from 'three';
 import NutGroup from '../geometry/NutGroup';
-import { parsePrimitiveColor, parsePrimitiveInfo, parsePrimitiveNodeId, parsePrimitiveTreeIndex } from './parseUtils';
-const type = 'nut';
-
+import { MatchingGeometries, parsePrimitiveColor, parsePrimitiveNodeId, parsePrimitiveTreeIndex } from './parseUtils';
 const color = new THREE.Color();
-const vector1 = new THREE.Vector3();
-const vector2 = new THREE.Vector3();
-const vector3 = new THREE.Vector3();
+const centerA = new THREE.Vector3();
+const centerB = new THREE.Vector3();
 
-function count(geometries: any[]): number {
-  return geometries.reduce(
-    (total, geometry) => { return geometry.type === type ? total + 1 : total; }, 0);
+function findMatchingGeometries(geometries: any[]): MatchingGeometries {
+  const matchingGeometries: MatchingGeometries = {
+    count: 0,
+    geometries: [],
+  };
+
+  geometries.forEach(geometry => {
+    if (geometry.type === 'nut') {
+      matchingGeometries.geometries.push(geometry);
+      matchingGeometries.count += 1;
+    }
+  });
+
+  return matchingGeometries;
 }
 
 export default function parse(geometries: any[]): NutGroup|null {
-  const group = new NutGroup(count(geometries));
+  const matchingGeometries = findMatchingGeometries(geometries);
+  const group = new NutGroup(matchingGeometries.count);
   if (group.capacity === 0) {
     return null;
   }
-  const objects = geometries.filter(geometry => geometry.type === type);
 
-  objects.forEach(geometry => {
-    const primitiveInfo = parsePrimitiveInfo(geometry.primitiveInfo);
+  matchingGeometries.geometries.forEach(geometry => {
+    const primitiveInfo = geometry.primitiveInfo[geometry.type];
 
     const nodeId = parsePrimitiveNodeId(geometry);
     const treeIndex = parsePrimitiveTreeIndex(geometry);
     color.setHex(parsePrimitiveColor(geometry));
 
-    const centerA = geometry.primitiveInfo[geometry.type].centerA;
-    const centerB = geometry.primitiveInfo[geometry.type].centerB;
-    const radius = geometry.primitiveInfo[geometry.type].radius;
-    const rotationAngle = geometry.primitiveInfo[geometry.type].rotationAngle;
+    centerA.set(primitiveInfo.centerA.x, primitiveInfo.centerA.y, primitiveInfo.centerA.z);
+    centerB.set(primitiveInfo.centerB.x, primitiveInfo.centerB.y, primitiveInfo.centerB.z);
+    const radius = primitiveInfo.radius;
+    const rotationAngle = primitiveInfo.rotationAngle;
 
-    vector1.set(centerA.x, centerA.y, centerA.z);
-    vector2.set(centerB.x, centerB.y, centerB.z);
     group.add(nodeId, treeIndex, color, centerA, centerB, radius, rotationAngle);
   });
+
   return group;
 }
