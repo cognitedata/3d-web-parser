4798d598a48d146e3866d5022b4d6b35a44f6286
Anders Hafreager <anders.hafreager@cognite.com>
2019-01-25 09:50:51 +0100

    Exporting geometry groups

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 // Copyright 2019 Cognite AS
 export { default as Sector } from './Sector';
 export { default as parseProtobuf } from './Parser';
+
+export { default as BaseCylinderGroup } from './geometry/BaseCylinderGroup';
+export { default as BoxGroup } from './geometry/BoxGroup';
+export { default as CircleGroup } from './geometry/CircleGroup';
+export { default as ConeGroup } from './geometry/ConeGroup';
+export { default as EccentricConeGroup } from './geometry/EccentricConeGroup';
+export { default as EllipsoidSegmentGroup } from './geometry/EllipsoidSegmentGroup';
+export { default as ExtrudedRingGroup } from './geometry/ExtrudedRingGroup';
+export { default as GeneralCylinderGroup } from './geometry/GeneralCylinderGroup';
+export { default as GeneralRingGroup } from './geometry/GeneralRingGroup';
+export { default as GeometryGroup } from './geometry/GeometryGroup';
+export { default as MergedMeshGroup } from './geometry/MergedMeshGroup';
+export { default as NutGroup } from './geometry/NutGroup';
+export { default as PlaneGroup } from './geometry/PlaneGroup';
+export { default as PrimitiveGroup } from './geometry/PrimitiveGroup';
+export { default as QuadGroup } from './geometry/QuadGroup';
+export { default as SphericalSegmentGroup } from './geometry/SphericalSegmentGroup';
+export { default as TorusSegmentGroup } from './geometry/TorusSegmentGroup';
+export { default as TrapeziumGroup } from './geometry/TrapeziumGroup';
