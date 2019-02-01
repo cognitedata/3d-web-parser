aa25a46007a3f6429104e7eb02708f068d2a57af
Verkhovskaya <verkhovskaya.anna@gmail.com>
2019-02-01 14:52:43 +0100

    fibonacci decoder

---
+++ src/__tests__/customFileParserTest/FibonacciDecoderTest.ts
@@ -0,0 +1,27 @@
+import FibonacciDecoder from '../../customFileParser/FibonacciDecoder';
+
+describe('FibonacciDecoder', () => {
+  test('parse fibonacci encoding', async() => {
+    const uncompressedData = new ArrayBuffer(13);
+    const uncompressedView = new Uint8Array(uncompressedData);
+    uncompressedView[0] = parseInt('11110011', 2);
+    uncompressedView[1] = parseInt('10001000', 2);
+    uncompressedView[2] = parseInt('10001000', 2);
+    uncompressedView[3] = parseInt('00000010', 2);
+    uncompressedView[4] = parseInt('00101001', 2);
+    uncompressedView[5] = parseInt('00000001', 2);
+    uncompressedView[6] = parseInt('01000000', 2);
+    uncompressedView[7] = parseInt('10010001', 2);
+    uncompressedView[8] = parseInt('00010101', 2);
+    uncompressedView[9] = parseInt('00000010', 2);
+    uncompressedView[10] = parseInt('01011110', 2);
+    uncompressedView[11] = parseInt('10110000', 2);
+    const decoder = new FibonacciDecoder(uncompressedData);
+    expect(decoder.nextValue()).toBe(0);
+    expect(decoder.nextValue()).toBe(0);
+    expect(decoder.nextValue()).toBe(2);
+    expect(decoder.nextValue()).toBe(8164540613673614);
+    expect(decoder.nextValue()).toBe(0);
+    expect(decoder.nextValue()).toBe(6);
+  });
+});
---
+++ src/customFileParser/FibonacciDecoder.ts
@@ -0,0 +1,59 @@
+const fibonacciLookup = [1, 2];
+for (let i = 2; i <= 77; i++) {
+  fibonacciLookup.push(fibonacciLookup[i - 1] + fibonacciLookup[i - 2]);
+}
+
+export default class FibonacciDecoder {
+  public numberRead: number;
+
+  private buffer: ArrayBuffer;
+  private data: Uint8Array;
+
+  private readBitId: number;
+  private currentValue: number;
+  private nextFibIndex: number;
+
+  constructor(compressedBuffer: ArrayBuffer, startByte?: number, length?: number) {
+    if (!startByte) { startByte = 0; }
+    if (!length) { length = compressedBuffer.byteLength - startByte; }
+
+    this.numberRead = 0;
+    this.buffer = compressedBuffer;
+    this.data = new Uint8Array(this.buffer, startByte, length);
+
+    // read first bit
+    this.currentValue = (this.data[0] & (1 << 7)) >> 7;
+    this.nextFibIndex = 1;
+    this.readBitId = 1;
+  }
+
+  // Read next encoded value
+  nextValue() {
+    this.numberRead++;
+
+    // read rest of file
+    for (let i = 0; i < 100; i++) {
+      const currentBit8 = this.data[Math.floor(this.readBitId / 8)] & 1 << (7 - (this.readBitId % 8));
+      if (currentBit8 !== 0) {
+
+        // check if termination bit
+        const previousBit8 = this.data[Math.floor((this.readBitId - 1) / 8)] & (1 << (7 - (this.readBitId - 1) % 8));
+        if ((previousBit8 !== 0) && (this.nextFibIndex !== 0)) {
+          const returnValue = this.currentValue - 1;
+          this.currentValue = 0;
+          this.nextFibIndex = 0;
+          this.readBitId++;
+          return returnValue;
+
+        // Otherwise update read value
+        } else {
+          this.currentValue += fibonacciLookup[this.nextFibIndex];
+        }
+      }
+      this.nextFibIndex++;
+      this.readBitId++;
+    }
+
+    throw Error('THIS IS REALLY BAD');
+  }
+}
