ec8461811ef455b59fe209788c1d9f53306ab596
Verkhovskaya <verkhovskaya.anna@gmail.com>
2019-03-08 10:29:45 +0100

    Started preloading mesh files

--- src/customFileParser/main.ts
+++ src/customFileParser/main.ts
@@ -4,16 +4,22 @@ import CustomFileReader from './CustomFileReader';
 import SceneStats from './../SceneStats';
 import { CompressedGeometryData } from './sharedFileParserTypes';
 
-export default function parseCustomFile(fileBuffer: ArrayBuffer) {
+function preloadMeshFiles(meshLoader: any, fileIds: number[]) {
+  fileIds.forEach(fileId => {
+    meshLoader.getGeometry(fileId);
+  });
+}
+
+export default async function parseCustomFile(fileBuffer: ArrayBuffer, meshLoader: any) {
   const fileReader = new CustomFileReader(fileBuffer);
   const idToSectorMap: {[name: string]: Sector} = {};
   const sceneStats: SceneStats = {
     numInstancedMeshes: 0,
     numMergedMeshes: 0,
   };
+  const mergedMeshMap: any = {};
   const treeIndexNodeIdMap: number[] = [];
   const colorMap: THREE.Color[] = [];
-
   let rootSector = undefined;
   let uncompressedValues = undefined;
   const sectorPathToPrimitiveData: {[path: string]: CompressedGeometryData[]} = {};
@@ -30,6 +36,7 @@ export default function parseCustomFile(fileBuffer: ArrayBuffer) {
     if (rootSector === undefined || uncompressedValues === undefined) {
       rootSector = sector;
       uncompressedValues = fileReader.readUncompressedValues();
+      preloadMeshFiles(meshLoader, uncompressedValues.fileId!);
     } else {
       const parentSector = idToSectorMap[sectorMetadata.parentSectorId];
       if (parentSector !== undefined) {
