ec2d82e7c93d5d9e0760c46ff24b79c2db99f811
Verkhovskaya <verkhovskaya.anna@gmail.com>
2019-03-14 11:15:19 +0100

    fixed calculateMemoryUsage

--- src/geometry/GeometryGroupData.ts
+++ src/geometry/GeometryGroupData.ts
@@ -110,23 +110,11 @@ export default class GeometryGroupData {
     this.count += 1;
   }
 
-  memoryUsage(usage: any) {
-    Object.keys(this).forEach(key => {
-      // @ts-ignore
-      if (ArrayBuffer.isView(this[key])) {
-        if (usage.byProperty[key] == null) {
-          usage.byProperty[key] = 0;
-        }
-        if (usage.byGeometry[this.type] == null) {
-          usage.byGeometry[this.type] = 0;
-        }
-        // @ts-ignore
-        usage.byGeometry[this.type] += this[key].byteLength;
-        // @ts-ignore
-        usage.byProperty[key] += this[key].byteLength;
-        // @ts-ignore
-        usage.total += this[key].byteLength;
-      }
+  calculateMemoryUsage() {
+    let usage = 0;
+    Object.keys(this.arrays).forEach(arrayName => {
+      const array = this.arrays[arrayName];
+      usage += array.length * array.BYTES_PER_ELEMENT;
     });
     return usage;
   }
