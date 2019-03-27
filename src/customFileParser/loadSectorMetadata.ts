fa7bcf75775ce9ac43a03c197cb3806a9580d441
Verkhovskaya <verkhovskaya.anna@gmail.com>
2019-03-27 15:56:55 +0100

    cleanup

--- src/customFileParser/loadSectorMetadata.ts
+++ src/customFileParser/loadSectorMetadata.ts
@@ -7,7 +7,8 @@ const MAGIC_BYTES = 0x46443349;
 export default function loadSectorMetadata(fileReader: CustomFileReader) {
   const magicBytes = fileReader.readUint32();
   if (magicBytes !== MAGIC_BYTES) {
-    throw Error('Start of sector file is incorrect. Expected 0x46443349, got ' + magicBytes.toString(16));
+    throw Error('Start of sector file is incorrect.Expected ' +
+      MAGIC_BYTES.toString(16) + ', got ' + magicBytes.toString(16));
   }
   const formatVersion = fileReader.readUint32();
   const optimizerVersion = fileReader.readUint32();
