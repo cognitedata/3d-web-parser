7a21d62e9faa0807e343e668821859d078afe6bd
verkhovskaya <Verkhovskaya@users.noreply.github.com>
2019-03-28 12:51:27 +0100

    Enable multiple meshes to have the same tree index (#26)

--- src/customFileParser/loadSectorMetadata.ts
+++ src/customFileParser/loadSectorMetadata.ts
@@ -2,8 +2,14 @@ import * as THREE from 'three';
 import CustomFileReader from './CustomFileReader';
 import { SectorMetadata } from './sharedFileParserTypes';
 
+const MAGIC_BYTES = 0x46443349;
+
 export default function loadSectorMetadata(fileReader: CustomFileReader) {
   const magicBytes = fileReader.readUint32();
+  if (magicBytes !== MAGIC_BYTES) {
+    throw Error('Start of sector file is incorrect.Expected ' +
+      MAGIC_BYTES.toString(16) + ', got ' + magicBytes.toString(16));
+  }
   const formatVersion = fileReader.readUint32();
   const optimizerVersion = fileReader.readUint32();
   const sectorId = fileReader.readUint64();
