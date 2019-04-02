6216bef2454eb18e5c352b953fa9dfd22b3f8c7c
verkhovskaya <Verkhovskaya@users.noreply.github.com>
2019-04-02 15:38:08 +0200

    Fix compute bounding box for merged and instanced mesh groups (#31)

--- src/geometry/GeometryUtils.ts
+++ src/geometry/GeometryUtils.ts
@@ -1,17 +1,7 @@
 import * as THREE from 'three';
 import { identityMatrix4 } from '../constants';
-import { zAxis } from '../constants';
 
 const globalVector = new THREE.Vector3();
-const globalCenter = new THREE.Vector3();
-const globalRotation = new THREE.Quaternion();
-const globalScale = new THREE.Vector3();
-const SI = new THREE.Matrix4();
-SI.set(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, -1);
-const globalMatrix = new THREE.Matrix4();
-const globalMatrix2 = new THREE.Matrix4();
-const globalArray = new Array(16);
-const globalSize = new THREE.Vector3();
 
 export function computeBoundingBox(
   box: THREE.Box3,
@@ -21,6 +11,7 @@ export function computeBoundingBox(
   triangleOffset: number,
   triangleCount: number,
 ) {
+  box.makeEmpty();
   const start = 3 * triangleOffset;
   const end = start + 3 * triangleCount;
   const { array } = index;
