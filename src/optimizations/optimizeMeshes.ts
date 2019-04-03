1c5d2b2a28985f8945f651659bd7ed773c979d82
Verkhovskaya <verkhovskaya.anna@gmail.com>
2019-04-03 15:11:58 +0200

    cleanup

--- src/optimizations/optimizeMeshes.ts
+++ src/optimizations/optimizeMeshes.ts
@@ -9,51 +9,6 @@ const globalMatrix = new THREE.Matrix4();
 
 const TRIANGLE_COUNT_LIMIT = 10000;
 
-function countTriangles(sector: Sector) {
-  const instancedMeshes: {[fileId: string]: {[triangleOffset: string]: number}} = {};
-  const mergedMeshes: {[fileId: string]: number} = {};
-  sector.instancedMeshGroup.meshes.forEach(mesh => {
-    instancedMeshes[mesh.fileId] = instancedMeshes[mesh.fileId] ? instancedMeshes[mesh.fileId] : {};
-    mesh.collections.forEach(collection => {
-      instancedMeshes[mesh.fileId][collection.triangleOffset] =
-        (collection.mappings.count - 1) * collection.triangleCount;
-    });
-  });
-
-  sector.mergedMeshGroup.meshes.forEach(mesh => {
-    if (mesh.mappings.triangleCounts.length > 0) {
-      mergedMeshes[mesh.fileId] = mesh.mappings.triangleCounts.reduce((a, b) => a + b);
-    }
-  });
-
-  return { instancedMeshes, mergedMeshes };
-}
-
-export function meshStatusReport(rootSector: Sector) {
-  const instancedMeshCounts: {[fileId: string]: {[sectorPath: string]:
-    {[triangleOffset: string]: number}}} = {};
-  const mergedMeshCounts: {[fileId: string]: {[sectorPath: string]: number}} = {};
-  const perSectorBreakdown: {[sectorPath: string]: {
-    instancedMeshes: {[fileId: string]: {[triangleOffset: string]: number}},
-    mergedMeshes: {[fileId: string]: number},
-  }} = {};
-
-  for (const sector of rootSector.traverseSectors()) {
-    const triangeCount = countTriangles(sector);
-    perSectorBreakdown[sector.path] = triangeCount;
-    Object.keys(triangeCount.instancedMeshes).map((fileId: string) => {
-      instancedMeshCounts[fileId] = instancedMeshCounts[fileId] ? instancedMeshCounts[fileId] : {};
-      instancedMeshCounts[fileId][sector.path] = triangeCount.instancedMeshes[fileId];
-    });
-    Object.keys(triangeCount.mergedMeshes).map((fileId: string) => {
-      mergedMeshCounts[fileId] = mergedMeshCounts[fileId] ? mergedMeshCounts[fileId] : {};
-      mergedMeshCounts[fileId][sector.path] = triangeCount.mergedMeshes[fileId];
-    });
-  }
-
-  return { instancedMeshCounts, mergedMeshCounts, perSectorBreakdown };
-}
-
 function countMappingsToMerge(collections: InstancedMeshCollection[]) {
   let numMappings = 0;
   collections.forEach(collection => {
@@ -109,4 +64,49 @@ export function optimizeMeshes(
       }
     }
   }
+}
+
+function countTriangles(sector: Sector) {
+  const instancedMeshes: {[fileId: string]: {[triangleOffset: string]: number}} = {};
+  const mergedMeshes: {[fileId: string]: number} = {};
+  sector.instancedMeshGroup.meshes.forEach(mesh => {
+    instancedMeshes[mesh.fileId] = instancedMeshes[mesh.fileId] ? instancedMeshes[mesh.fileId] : {};
+    mesh.collections.forEach(collection => {
+      instancedMeshes[mesh.fileId][collection.triangleOffset] =
+        (collection.mappings.count - 1) * collection.triangleCount;
+    });
+  });
+
+  sector.mergedMeshGroup.meshes.forEach(mesh => {
+    if (mesh.mappings.triangleCounts.length > 0) {
+      mergedMeshes[mesh.fileId] = mesh.mappings.triangleCounts.reduce((a, b) => a + b);
+    }
+  });
+
+  return { instancedMeshes, mergedMeshes };
+}
+
+export function meshStatusReport(rootSector: Sector) {
+  const instancedMeshCounts: {[fileId: string]: {[sectorPath: string]:
+    {[triangleOffset: string]: number}}} = {};
+  const mergedMeshCounts: {[fileId: string]: {[sectorPath: string]: number}} = {};
+  const perSectorBreakdown: {[sectorPath: string]: {
+    instancedMeshes: {[fileId: string]: {[triangleOffset: string]: number}},
+    mergedMeshes: {[fileId: string]: number},
+  }} = {};
+
+  for (const sector of rootSector.traverseSectors()) {
+    const triangeCount = countTriangles(sector);
+    perSectorBreakdown[sector.path] = triangeCount;
+    Object.keys(triangeCount.instancedMeshes).map((fileId: string) => {
+      instancedMeshCounts[fileId] = instancedMeshCounts[fileId] ? instancedMeshCounts[fileId] : {};
+      instancedMeshCounts[fileId][sector.path] = triangeCount.instancedMeshes[fileId];
+    });
+    Object.keys(triangeCount.mergedMeshes).map((fileId: string) => {
+      mergedMeshCounts[fileId] = mergedMeshCounts[fileId] ? mergedMeshCounts[fileId] : {};
+      mergedMeshCounts[fileId][sector.path] = triangeCount.mergedMeshes[fileId];
+    });
+  }
+
+  return { instancedMeshCounts, mergedMeshCounts, perSectorBreakdown };
 }
