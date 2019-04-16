2152accebd42f51daf5f064fb10e3f5f73bba100
Verkhovskaya <verkhovskaya.anna@gmail.com>
2019-04-16 16:13:47 +0200

    cleanup

--- src/optimizations/mergeInstancedMeshes.ts
+++ src/optimizations/mergeInstancedMeshes.ts
@@ -24,7 +24,7 @@ export default function mergeInstancedMeshes(
       smallCollections.forEach((collection, collectionIndex) => {
         for (let mappingIndex = 0; mappingIndex < collection.mappings.capacity; mappingIndex++) {
           mappingsSortedBySize.push({
-            collectionIndex, mappingIndex, size: collection.mappings.getSize(i) });
+            collectionIndex, mappingIndex, size: collection.mappings.getSize(mappingIndex) });
         }
       });
       mappingsSortedBySize.sort((a: MappingInfo, b: MappingInfo) => a.size - b.size);
