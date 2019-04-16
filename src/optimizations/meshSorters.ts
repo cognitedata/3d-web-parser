9bb4d28832ce93e0cb6efd6edaccbedb7d291aac
Verkhovskaya <verkhovskaya.anna@gmail.com>
2019-04-16 13:20:04 +0200

    cleanup

--- src/optimizations/meshSorters.ts
+++ src/optimizations/meshSorters.ts
@@ -7,24 +7,26 @@ export class InstancedMeshCollectionSizeSorter {
 
   constructor(collections: InstancedMeshCollection[]) {
     this.collections = [...collections];
-    this.mappingIds = this.collections.map(mesh => 0);
+    this.mappingIds = this.collections.map(collection => 0);
   }
 
   getNext(): any {
-    const sizes = this.collections.map((mesh, meshIndex) =>
-      this.mappingIds[meshIndex] < mesh.mappings.count ? mesh.mappings.getSize(this.mappingIds[meshIndex]) : -1,
+    const sizes = this.collections.map((collection, collectionIndex) =>
+      this.mappingIds[collectionIndex] < collection.mappings.count ?
+        collection.mappings.getSize(this.mappingIds[collectionIndex]) : -1,
     );
     const maxSize = Math.max(...sizes);
-    const nextMeshIndex = sizes.indexOf(maxSize);
-    const nextCollection = this.collections[nextMeshIndex];
-    const nextMappingId = this.mappingIds[nextMeshIndex];
-    this.mappingIds[nextMeshIndex]++;
+    const nextCollectionIndex = sizes.indexOf(maxSize);
+    const nextCollection = this.collections[nextCollectionIndex];
+    const nextMappingId = this.mappingIds[nextCollectionIndex];
+    this.mappingIds[nextCollectionIndex]++;
 
     return { collection: nextCollection, mappingIndex: nextMappingId };
   }
 
   isNotEmpty(): boolean {
-    return this.collections.some((mesh, meshIndex) => this.mappingIds[meshIndex] < mesh.mappings.count);
+    return this.collections.some((collection, collectionIndex) =>
+      this.mappingIds[collectionIndex] < collection.mappings.count);
   }
 }
 
