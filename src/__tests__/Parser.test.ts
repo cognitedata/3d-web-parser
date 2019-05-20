8052afa22f37dd1248d29d6047a6ecf87d1bcd2c
Anders Hafreager <anders.hafreager@cognite.com>
2019-05-20 10:43:55 +0200

    Fixed tests

--- src/__tests__/Parser.test.ts
+++ src/__tests__/Parser.test.ts
@@ -7,15 +7,18 @@ import * as TestScene from './fixtures/test_scene.json';
 import { expectBoundingBoxEqual } from '../TestUtils';
 
 describe('Parser', () => {
-  test('parse sector', () => {
+  test('parse sector', done => {
     const protobufData = fs.readFileSync('./src/__tests__/fixtures/test_scene.pb', null);
-    const { rootSector, maps } = parse(protobufData);
-    expect(rootSector).toBeInstanceOf(Sector);
+    parse(protobufData).then(data => {
+      const { rootSector, maps } = data;
+      expect(rootSector).toBeInstanceOf(Sector);
 
-    // validate bounding box
-    const { boundingBox } = TestScene;
-    expectBoundingBoxEqual(rootSector, boundingBox);
-    expect(rootSector.parent).toBe(undefined);
-    expect(rootSector.children.length).toBe(0);
+      // validate bounding box
+      const { boundingBox } = TestScene;
+      expectBoundingBoxEqual(rootSector, boundingBox);
+      expect(rootSector.parent).toBe(undefined);
+      expect(rootSector.children.length).toBe(0);
+      done();
+    });
   });
 });
