09ada97c14fcc53b9f42d13b659c0982b1931d38
Svenn-Arne Dragly <dragly@cognite.com>
2019-08-29 18:48:57 +0200

    WIP parse files in Rust using the I3DF library

---
+++ rust/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+function resolve(dir) {
+}
+
+module.exports = {
+    entry: './index.js',
+    devServer: {
+      compress: true,
+      contentBase: path.join(__dirname, 'public'),
+      stats: 'minimal',
+      host: 'localhost',
+      https: true,
+      open: false,
+      overlay: {
+        warnings: false, // reports errors in browser window
+        errors: true,
+      },
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, ".")
+        }),
+        // Have this example work in Edge which doesn't ship `TextEncoder` or
+        // `TextDecoder` at this time.
+        new webpack.ProvidePlugin({
+          TextDecoder: ['text-encoding', 'TextDecoder'],
+          TextEncoder: ['text-encoding', 'TextEncoder']
+        })
+    ],
+    //mode: 'development',
+    mode: 'production'
+};
