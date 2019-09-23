c4f23be1a0a3e057cb56985bf7346380c842e5c3
Lars Moastuen <lars.moastuen@cognite.com>
2019-09-23 14:43:52 +0200

    SectorGeometryLoader.load() no longer returns Promise.

--- src/streaming/SectorGeometryLoader.ts
+++ src/streaming/SectorGeometryLoader.ts
@@ -7,5 +7,5 @@ import { SectorId } from './SectorManager';
  * Interface for low level classes for loading sector geometry.
  */
 export interface SectorGeometryLoader {
-  load(id: SectorId): Promise<SectorGeometry>;
+  load(id: SectorId): SectorGeometry;
 }
