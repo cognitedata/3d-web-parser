ed122ac3178785394b81fffa7120402252d05a14
Lars Moastuen <lars.moastuen@cognite.com>
2019-09-23 11:23:02 +0200

    id: number -> SectorId.

--- src/streaming/SectorGeometry.ts
+++ src/streaming/SectorGeometry.ts
@@ -3,9 +3,10 @@
 import { PrimitiveGroup } from '..';
 import { MergedMeshGroup } from '../geometry/MergedMeshGroup';
 import { InstancedMeshGroup } from '../geometry/InstancedMeshGroup';
+import { SectorId } from './SectorManager';
 
 export type SectorGeometry = {
-  id: number;
+  id: SectorId;
   primitiveGroups: PrimitiveGroup[];
   mergedMeshGroup?: MergedMeshGroup;
   instancedMeshGroup?: InstancedMeshGroup;
