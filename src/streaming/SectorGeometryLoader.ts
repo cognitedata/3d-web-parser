a93c10710ed5afa93385bcf787ba04d56c42c4b3
Lars Moastuen <lars.moastuen@cognite.com>
2019-09-30 14:17:58 +0200

    Doc

--- src/streaming/SectorGeometryLoader.ts
+++ src/streaming/SectorGeometryLoader.ts
@@ -1,35 +1,12 @@
 // Copyright 2019 Cognite AS
 
-import { SectorGeometry } from './SectorGeometry';
 import { SectorId } from './SectorManager';
 
 /**
- * Interface for low level classes for loading sector geometry.
+ * Interface for low level classes for loading data for sector geometry.
+ * Implementations will typically download the data from the web or look up
+ * from a local datastore..
  */
 export interface SectorGeometryLoader {
   load(id: SectorId): Promise<ArrayBuffer>;
 }
-
-// export class DefaultSectorGeometryLoader implements SectorGeometryLoader {
-//   async load(id: SectorId): Promise<SectorGeometry> {
-//     console.log(`Load ${id}`);
-//     try {
-//       const file = sectorFiles.get(id);
-//       if (!file) {
-//         throw new Error(`Sector ${id} does not have a file mapping`);
-//       }
-//       const buffer = await sdk.files3D.retrieve(file!.fileId);
-//       const { rootSector, sceneStats, maps } =
-//         file.version >= 4 ? parseMultipleCustomFiles([buffer], null) : await parseProtobuf([new Uint8Array(buffer)]);
-//       console.log(`Sector ${id} success`);
-//       return Promise.resolve<SectorGeometry>({
-//         id: rootSector.id,
-//         primitiveGroups: rootSector.primitiveGroups,
-//         instancedMeshGroup: rootSector.instancedMeshGroup
-//       });
-//     } catch (error) {
-//       console.log(`Failed to load sector ${id}: '${error}'`);
-//       throw error;
-//     }
-//   }
-// }
